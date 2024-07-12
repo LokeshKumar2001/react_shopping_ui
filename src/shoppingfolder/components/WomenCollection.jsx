e88e2874623dee8487ae204f25ef820f209ec255
Sai Lokesh <chigurusettisailokesh@gmail.com>
2024-07-12 11:17:12 +0530

    feat: shopping

---
+++ src/shoppingfolder/components/WomenCollection.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const WomenCollection = (props) => {
+  const {
+    title,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+  } = props.ladiesFashion;
+  return (
+    <div className="collectionSection">
+      <h2>{title}</h2>
+      <div className="bannerBox">
+        <img src="assets/LadiesBanner.gif" alt="" />
+      </div>
+
+      <div className="womenImages">
+        <img src={image1} alt={title} />
+        <img src={image2} alt={title} />
+        <img src={image3} alt={title} />
+        <img src={image4} alt={title} />
+        <img src={image5} alt={title} />
+        <img src={image6} alt={title} />
+        <img src={image7} alt={title} />
+        <img src={image8} alt={title} />
+      </div>
+    </div>
+  );
+};
+
+export default WomenCollection;
